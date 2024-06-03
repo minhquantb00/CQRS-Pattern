060cfc0e2b00e3c0ebbbe89495dd1a717401aa1e
minhquantb00 <minhquantb00@gmail.com>
2024-06-03 14:49:34 +0700

    init

---
+++ src/Domain/Users/UserErrors.cs
@@ -0,0 +1,9 @@
+﻿using SharedKernel;
+
+namespace Domain.Users;
+
+public static class UserErrors
+{
+    public static Error NotFound(Guid userId) => new(
+        "Users.NotFound", $"The user with the Id = '{userId}' was not found");
+}
